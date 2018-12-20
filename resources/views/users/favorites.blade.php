e45d2e7a60162117f82587d2d8785cdd4af51908
y.k <ec2-user@ip-172-31-29-208.ec2.internal>
2018-12-20 10:14:41 +0000

    lesson15 last kadai fixed

--- resources/views/users/favorites.blade.php
+++ resources/views/users/favorites.blade.php
@@ -11,13 +11,18 @@
         <aside class="col-sm-4">
             @include('users.card', ['user' => $user])
         </aside>
-        <div class="col-sm-8">
-            @include('users.navtabs', ['user' => $user])
 
+        <div class="col-sm-8">
+            <ul class="nav nav-justified mb-3">
+                <li class="nav-item"><a class="nav-link" href="{{ route('users.show', ['id' => $user->id]) }}" class="nav-link {{ Request::is('users/' . $user->id) ? 'active' : '' }}">TimeLine <span class="badge badge-secondary">{{ $count_microposts }}</span></a></li>
+                <li class="nav-item"><a class="nav-link" href="{{ route('users.followings', ['id' => $user->id]) }}" class="nav-link {{ Request::is('users/*/followings') ? 'active' : '' }}">Followings <span class="badge badge-secondary">{{ $count_followings }}</span></a></li>
+                <li class="nav-item"><a class="nav-link" href="{{ route('users.followers', ['id' => $user->id]) }}" class="nav-link {{ Request::is('users/*/followers') ? 'active' : '' }}">Followers <span class="badge badge-secondary">{{ $count_followers }}</span></a></li>
+                <li class="nav-item"><a class="nav-link disabled" href="{{ route('favorites.favorites', ['id' => $user->id]) }}" class="nav-link {{ Request::is('users/*/favorites') ? 'active' : '' }}">Favorites <span class="badge badge-secondary">{{ $count_favorites }}</span></a></li>
+            </ul>
             @if (count($microposts) > 0)
                 @include('microposts.microposts', ['microposts' => $microposts])
             @endif
-            
         </div>
+
     </div>
 @endsection
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,4 +1,4 @@
-<!-- タブ"My profile"->トップ画面全体 -->
+<!-- タブ"My profile"->トップ画面全体--(タイムライン一覧) -->
 
 @extends('layouts.app')
 
@@ -8,7 +8,12 @@
             @include('users.card', ['user' => $user])
         </aside>
         <div class="col-sm-8">
-            @include('users.navtabs', ['user' => $user])
+            <ul class="nav nav-justified mb-3">
+                <li class="nav-item"><a class="nav-link disabled" href="{{ route('users.show', ['id' => $user->id]) }}" class="nav-link {{ Request::is('users/' . $user->id) ? 'active' : '' }}">TimeLine <span class="badge badge-secondary">{{ $count_microposts }}</span></a></li>
+                <li class="nav-item"><a class="nav-link" href="{{ route('users.followings', ['id' => $user->id]) }}" class="nav-link {{ Request::is('users/*/followings') ? 'active' : '' }}">Followings <span class="badge badge-secondary">{{ $count_followings }}</span></a></li>
+                <li class="nav-item"><a class="nav-link" href="{{ route('users.followers', ['id' => $user->id]) }}" class="nav-link {{ Request::is('users/*/followers') ? 'active' : '' }}">Followers <span class="badge badge-secondary">{{ $count_followers }}</span></a></li>
+                <li class="nav-item"><a class="nav-link" href="{{ route('favorites.favorites', ['id' => $user->id]) }}" class="nav-link {{ Request::is('users/*/favorites') ? 'active' : '' }}">Favorites <span class="badge badge-secondary">{{ $count_favorites }}</span></a></li>
+            </ul>
             @if (Auth::id() == $user->id)
                 {!! Form::open(['route' => 'microposts.store']) !!}
                     <div class="form-group">
